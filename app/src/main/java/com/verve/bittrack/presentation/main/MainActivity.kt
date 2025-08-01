2d2f5a67b7384a0fe8080a80cd931d6c255e83c0
GigihDC <112244673+GigihDC@users.noreply.github.com>
2025-08-01 10:31:19 +0700

    Finishing Main

---
+++ app/src/main/java/com/verve/bittrack/presentation/main/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.verve.bittrack.presentation.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.verve.bittrack.R
+import com.verve.bittrack.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+        setupBottomNav()
+        setOnclickAction()
+    }
+
+    private fun setOnclickAction() {
+        binding.fab.setOnClickListener {
+            navigateToFavorite()
+        }
+    }
+
+    private fun setupBottomNav() {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    private fun navigateToFavorite() {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        binding.navView.selectedItemId = R.id.menu_tab_favorite
+        navController.navigate(R.id.menu_tab_favorite)
+    }
+}
